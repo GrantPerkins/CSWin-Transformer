61c45c7b7d3ed4871db4464aae072f924bf4913a
GrantPerkins <gcperk20@gmail.com>
2023-11-01 18:05:04 -0400

    one grad per channel

--- finetune.py
+++ finetune.py
@@ -757,6 +757,8 @@ def grad(model, loader, dataset, args, amp_autocast=suppress):
                 print(result.shape)
                 result[:, torch.argmax(result,dim=1)].backward()
                 gradients = model.get_activations_gradient()
+                for grad in gradients:
+                    print(grad.cpu().size())
                 print(gradients)
                 print("gradients shape", np.array(gradients.cpu()).shape)
                 activations = model.get_activations().detach()
