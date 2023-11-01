602d68d9cd6fa7113bcc5940e91b7b5a1d6c8c61
GrantPerkins <gcperk20@gmail.com>
2023-11-01 17:49:40 -0400

    Move from GPU

--- finetune.py
+++ finetune.py
@@ -757,7 +757,7 @@ def grad(model, loader, dataset, args, amp_autocast=suppress):
                 print(result.shape)
                 result[:, torch.argmax(result,dim=1)].backward()
                 gradients = model.get_activations_gradient()
-                print("gradients shape", np.array(gradients).shape)
+                print("gradients shape", np.array(gradients.cpu()).shape)
                 activations = model.get_activations().detach()
 
             break
